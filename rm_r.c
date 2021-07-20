eadbcbc564296672a81e6e96b6f37470560f6e38
Giovanni Bechis <gbechis@pccc.com>
2021-07-20 22:41:12 +0200

    Cleanup rm_r function (#40)
    * use only C code to remove directory entries without using execl(3), this was only needed on IRIX and on now unsupported platforms
    
    * use NAME_MAX
    
    * switch to readdir(3), readdir_r(3) is deprecated

--- rm_r.c
+++ rm_r.c
@@ -24,10 +24,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#ifdef EXEC_RM_FOR_CLEANUP
-#include <sys/wait.h>
-#endif
-
 
 #ifdef ENABLE_DEBUGGING
 extern void *malloc_debug(void *, size_t, char const *fname, int);
@@ -43,7 +39,6 @@ extern void free_debug(void *, void *, char const *, int);
 #define strdup_with_log(x) strdup_debug(ctx, x, __FILE__, __LINE__)
 #endif
 
-#ifndef EXEC_RM_FOR_CLEANUP
 /**********************************************************************
 * %FUNCTION: rm_r
 * %ARGUMENTS:
@@ -60,26 +55,25 @@ rm_r(char const *qid, char const *dir)
     struct stat sbuf;
     DIR *d;
     struct dirent *entry;
-    struct dirent *result = NULL;
-    int n;
     int retcode = 0;
+    int errno_save;
 
     if (!qid || !*qid) {
-	qid = "NOQUEUE";
+      qid = "NOQUEUE";
     }
 
     if (lstat(dir, &sbuf) < 0) {
-	syslog(LOG_WARNING, "%s: lstat(%s) failed: %m", qid, dir);
-	return -1;
+      syslog(LOG_WARNING, "%s: lstat(%s) failed: %m", qid, dir);
+      return -1;
     }
 
     if (!S_ISDIR(sbuf.st_mode)) {
-	/* Not a directory - just unlink */
-	if (unlink(dir) < 0) {
-	    syslog(LOG_WARNING, "%s: unlink(%s) failed: %m", qid, dir);
-	    return -1;
-	}
-	return 0;
+      /* Not a directory - just unlink */
+      if (unlink(dir) < 0) {
+	      syslog(LOG_WARNING, "%s: unlink(%s) failed: %m", qid, dir);
+	      return -1;
+	    }
+	    return 0;
     }
 
     /* Allocate room for entry */
@@ -87,91 +81,38 @@ rm_r(char const *qid, char const *dir)
     entry = (struct dirent *) malloc(sizeof(struct dirent) + pathconf(dir, _PC_NAME_MAX) + 1);
 #else
     /* Can't use _POSIX_NAME_MAX because it's often defined as 14...
-       useless... */
-    entry = (struct dirent *) malloc(sizeof(struct dirent) + 257);
+       use NAME_MAX instead */
+    entry = (struct dirent *) malloc(sizeof(struct dirent) + NAME_MAX);
 #endif
     if (!entry) {
-	syslog(LOG_WARNING, "%s: Unable to allocate space for dirent entry: %m", qid);
-	return -1;
+      syslog(LOG_WARNING, "%s: Unable to allocate space for dirent entry: %m", qid);
+      return -1;
     }
 
     d = opendir(dir);
     if (!d) {
-	int errno_save = errno;
-	syslog(LOG_WARNING, "%s: opendir(%s) failed: %m", qid, dir);
-	free(entry);
-	errno = errno_save;
-	return -1;
+      errno_save = errno;
+      syslog(LOG_WARNING, "%s: opendir(%s) failed: %m", qid, dir);
+      free(entry);
+      errno = errno_save;
+      return -1;
     }
 
-    for (;;) {
-	n = readdir_r(d, entry, &result);
-	if (n != 0) {
-	    errno = n;
-	    syslog(LOG_WARNING, "%s: readdir_r failed: %m", qid);
-	    closedir(d);
-	    free(entry);
-	    errno = n;
-	    return -1;
-	}
-	if (!result) break;
-	if (!strcmp(entry->d_name, ".") ||
-	    !strcmp(entry->d_name, "..")) {
-	    continue;
-	}
-	snprintf(buf, sizeof(buf), "%s/%s", dir, entry->d_name);
-	if (rm_r(qid, buf) < 0) {
-	    retcode = -1;
-	}
+    while((entry = readdir(d)) != NULL) {
+	    if (!strcmp(entry->d_name, ".") ||
+	      !strcmp(entry->d_name, "..")) {
+	      continue;
+	    }
+	    snprintf(buf, sizeof(buf), "%s/%s", dir, entry->d_name);
+	    if (rm_r(qid, buf) < 0) {
+	      retcode = -1;
+	    }
     }
     free(entry);
     closedir(d);
     if (rmdir(dir) < 0) {
-	syslog(LOG_WARNING, "%s: rmdir(%s) failed: %m", qid, dir);
-	return -1;
+      syslog(LOG_WARNING, "%s: rmdir(%s) failed: %m", qid, dir);
+      return -1;
     }
     return retcode;
 }
-
-#else /* EXEC_RM_FOR_CLEANUP */
-/**********************************************************************
-* %FUNCTION: rm_r
-* %ARGUMENTS:
-*  dir -- directory or file name
-* %RETURNS:
-*  -1 on error, 0 otherwise.
-* %DESCRIPTION:
-*  Deletes dir and recursively deletes contents.  Does so by forking and
-*  exec'ing "rm".
-***********************************************************************/
-int
-rm_r(char const *qid, char const *dir)
-{
-    pid_t pid;
-
-    if (!qid || !*qid) {
-	qid = "NOQUEUE";
-    }
-
-
-    pid = fork();
-    if (pid == (pid_t) -1) {
-	syslog(LOG_WARNING, "%s: fork() failed -- cannot clean up: %m", qid);
-	return -1;
-    }
-
-    if (pid == 0) {
-	/* In the child */
-	execl(RM, "rm", "-r", "-f", dir, NULL);
-	_exit(EXIT_FAILURE);
-    }
-
-    /* In the parent */
-    if (waitpid(pid, NULL, 0) < 0) {
-	syslog(LOG_ERR, "%s: waitpid() failed -- cannot clean up: %m", qid);
-	return -1;
-    }
-
-    return 0;
-}
-#endif  /* EXEC_RM_FOR_CLEANUP */
