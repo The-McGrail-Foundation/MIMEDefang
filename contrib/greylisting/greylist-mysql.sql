74ca47344c43bd4d5dff0568405fa06aa40f937a
Giovanni <giovanni@paclan.it>
2024-06-10 11:07:55 +0200

    store timestamps as int [skip ci]

--- contrib/greylisting/greylist-mysql.sql
+++ contrib/greylisting/greylist-mysql.sql
@@ -3,8 +3,8 @@ CREATE TABLE greylist
     sender_host_ip VARCHAR(40)  NOT NULL,
     sender         VARCHAR(256) NOT NULL,
     recipient      VARCHAR(256) NOT NULL,
-    first_received DATETIME     NOT NULL,
-    last_received  DATETIME     NOT NULL,
+    first_received INT(11)      NOT NULL,
+    last_received  INT(11)      NOT NULL,
     known_ip       TINYINT(1)   UNSIGNED NOT NULL,
     UNIQUE (sender, recipient, sender_host_ip)
 );
